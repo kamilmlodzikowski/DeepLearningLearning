df0dfd6026682b6e3bfa0471d0f9f6896622d25e
Justyna <j.ataman@tlen.pl>
2019-04-16 22:50:16 +0200

    działa dice_predict.py

--- dice/dice_predict.py
+++ dice/dice_predict.py
@@ -8,18 +8,16 @@ DATADIR = "valid"
 
 CATEGORIES = ["d4", "d6", "d8","d10","d12", "d20"]
 
+
 dobre=0
 calosc=0
 
 def prepare(filepath):
-    IMG_SIZE = 70  
-    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    IMG_SIZE = 100 
+    img_array = cv2.imread(filepath)
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-
- 
-        
-
+    new_array/255.0
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 3)
 
 model = tf.keras.models.load_model("dice6-CNN.model")
 
@@ -45,12 +43,7 @@ for category in CATEGORIES:
         except ValueError:
             calosc-=1
             pass
-            
-    
 
-
-
-        
 print("Dobre ", dobre, " na ", calosc)
 procent= (float(dobre)/float(calosc))*100.0
 print(numpy.round(procent,1), "%")
@@ -62,4 +55,4 @@ numpy.around(przewi,0)
 try:
     print(CATEGORIES[przewi.index(1)])
 except ValueError:
-    pass
+	pass
